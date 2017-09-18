cf29278f34dde91ba6d2f796c0e52c2e94b25096
Andrew Kramer <1988kramer@gmail.com>
2017-09-18 16:27:03 -0600

    started work on EKF with known landmark correspondence

--- common/loadMRCLAMdataSet.m
+++ common/loadMRCLAMdataSet.m
@@ -4,38 +4,40 @@
 % Description: This scripts parses the 17 text files that make up a 
 % dataset into Matlab arrays. Run this script within the the dataset 
 % directory.
+function loadMRCLAMdataSet(n_robots);
 
-n_robots = 3;
+    n_robots = 3; % set to any value from 1-5 to set the number of robots
 
-disp('Parsing Dataset')
-disp('Reading barcode numbers')
-[subject_num, barcode_num] = textread('Barcodes.dat', '%u %u','commentstyle','shell');
-Barcodes = [subject_num, barcode_num];
-clear subject_num barcode_num;
+    disp('Parsing Dataset')
+    disp('Reading barcode numbers')
+    [subject_num, barcode_num] = textread('Barcodes.dat', '%u %u','commentstyle','shell');
+    Barcodes = [subject_num, barcode_num];
+    clear subject_num barcode_num;
 
-disp('Reading landmark groundtruth')
-[subject_num x y x_sd y_sd] = textread('Landmark_Groundtruth.dat', '%f %f %f %f %f','commentstyle','shell');
-Landmark_Groundtruth = [subject_num x y x_sd y_sd];
-clear subject_num x y x_sd y_sd;
-n_landmarks = length(Landmark_Groundtruth); 
+    disp('Reading landmark groundtruth')
+    [subject_num x y x_sd y_sd] = textread('Landmark_Groundtruth.dat', '%f %f %f %f %f','commentstyle','shell');
+    Landmark_Groundtruth = [subject_num x y x_sd y_sd];
+    clear subject_num x y x_sd y_sd;
+    n_landmarks = length(Landmark_Groundtruth); 
 
-for i=1:n_robots
- 
-    disp(['Reading robot ' num2str(i) ' groundtruth'])
-    [time x y theta] = textread(['Robot' num2str(i) '_Groundtruth.dat'], '%f %f %f %f','commentstyle','shell');
-    eval(['Robot' num2str(i) '_Groundtruth = [time x y theta];']); 
-    clear time x y theta;
+    for i=1:n_robots
 
-    disp(['Reading robot ' num2str(i) ' odometry'])
-    [time, v, w] = textread(['Robot' num2str(i) '_Odometry.dat'], '%f %f %f','commentstyle','shell');
-    eval(['Robot' num2str(i) '_Odometry = [time v w];']);
-    clear time v w;
-    
-    disp(['Reading robot ' num2str(i) ' measurements'])
-    [time, barcode_num, r b] = textread(['Robot' num2str(i) '_Measurement.dat'], '%f %f %f %f','commentstyle','shell');
-    eval(['Robot' num2str(i) '_Measurement = [time barcode_num r b];']);
-    clear time barcode_num r b;
+        disp(['Reading robot ' num2str(i) ' groundtruth'])
+        [time x y theta] = textread(['Robot' num2str(i) '_Groundtruth.dat'], '%f %f %f %f','commentstyle','shell');
+        eval(['Robot' num2str(i) '_Groundtruth = [time x y theta];']); 
+        clear time x y theta;
 
+        disp(['Reading robot ' num2str(i) ' odometry'])
+        [time, v, w] = textread(['Robot' num2str(i) '_Odometry.dat'], '%f %f %f','commentstyle','shell');
+        eval(['Robot' num2str(i) '_Odometry = [time v w];']);
+        clear time v w;
+
+        disp(['Reading robot ' num2str(i) ' measurements'])
+        [time, barcode_num, r b] = textread(['Robot' num2str(i) '_Measurement.dat'], '%f %f %f %f','commentstyle','shell');
+        eval(['Robot' num2str(i) '_Measurement = [time barcode_num r b];']);
+        clear time barcode_num r b;
+
+    end
+    clear i
+    disp('Parsing Complete')
 end
-clear i
-disp('Parsing Complete')
---
+++ localization/EKF_known_corr.m
@@ -0,0 +1,7 @@
+addpath ../common;
+addpath ../MRCLAM_Dataset1;
+
+n_robots = 1;
+loadMRCLAMdataSet.m(n_robots);
+
+% rest of the code goes here
