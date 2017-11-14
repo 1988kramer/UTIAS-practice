5837636a680824dc7322cd90095c051a56f599d0
Andrew Kramer <1988kramer@gmail.com>
2017-11-14 15:35:31 -0700

    switched persistence filter to log probability

--- feature-persistence/persistence_filter.m
+++ feature-persistence/persistence_filter.m
@@ -1,9 +1,5 @@
 function [probs] = persistence_filter(pM, pF, lambda, marks_in_FOV, ...
-                                             probs, z, t, pV)
-    % calculate prior probability of landmark
-    % persistence at time t
-    nextPrior = -exp(-lambda * t) + 1; 
-    
+                                             probs, z, t, pV) 
     % loop over all landmark probabilities
     % update probabilities based on measurements and time elapsed
     % assumes correspondence IS KNOWN
@@ -23,18 +19,20 @@ function [probs] = persistence_filter(pM, pF, lambda, marks_in_FOV, ...
             if probs(i,6) > 0
                 % compute next prior for given landmark
                 nextPrior = -exp(-lambda * (t - probs(i,6))) + 1;
-                % compute partial evidence
-                probs(i,2) = ((pF^yT) * (1 - pF)^(1-yT)) ...
-                             * (probs(i,2) ...
-                             + (probs(i,3) * (nextPrior - probs(i,5))));
-                % compute likelihood
-                probs(i,3) = (pM^(1 - yT)) * ((1 - pM)^yT) * probs(i,3);
-                % compute evidence
-                probs(i,4) = probs(i,2) + probs(i,3) * (1 - nextPrior);
+                % compute ln(partial) evidence
+                probs(i,2) = ln((pF^yT) * (1 - pF)^(1-yT)) ...
+                             + probs(i,2) ...
+                             + ln(1 + exp(probs(i,3) + ln(nextPrior - probs(i,5)) - probs(i,2)));
+                % compute ln(likelihood)
+                probs(i,3) = ln((pM^(1 - yT)) * ((1 - pM)^yT)) + probs(i,3);
+                % compute ln(evidence)
+                probs(i,4) = probs(i,2) + ln(1 + exp(probs(i,3) - (lambda * t) - probs(i,2)));
                 % update prior
                 probs(i,5) = nextPrior;
-                % compute posterior probability
-                probs(i,1) = (probs(i,3) / probs(i,4)) * (1 - nextPrior);
+                % compute ln(posterior)
+                probs(i,1) = probs(i,3) - probs(i,4) - (lambda * t);
+                % convert ln(posterior) to posterior
+                probs(i,1) = exp(probs(i,1));
                 
                 % if persistence probability is less than threshold
                 % remove from map
