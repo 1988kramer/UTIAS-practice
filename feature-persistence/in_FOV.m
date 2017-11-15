f7a28d7432cc83c76b7398d80ea847c07215c6c8
Andrew Kramer <1988kramer@gmail.com>
2017-11-15 13:37:44 -0700

    added min range to in_FOV

--- feature-persistence/in_FOV.m
+++ feature-persistence/in_FOV.m
@@ -1,8 +1,10 @@
-function landmarks_in_FOV = in_FOV(predZ, maxBear, maxRange)
+function landmarks_in_FOV = in_FOV(predZ, maxBear, minRange, maxRange)
     n_landmarks = size(predZ, 2);
     landmarks_in_FOV = zeros(n_landmarks, 1);
     for i = 1:n_landmarks
-        if (predZ(1,i) < maxRange) && (abs(predZ(2,i)) < maxBear)
+        if predZ(1,i) < maxRange ...
+                && predZ(1,i) > minRange ...
+                && (abs(predZ(2,i)) < maxBear)
             landmarks_in_FOV(i) = 1;
         end
     end
