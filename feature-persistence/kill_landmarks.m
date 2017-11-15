0534569e125972e5c8c91b6ee87a68af871770fe
Andrew Kramer <1988kramer@gmail.com>
2017-11-15 16:22:57 -0700

    edited commint in kill_landmarks.m

--- feature-persistence/kill_landmarks.m
+++ feature-persistence/kill_landmarks.m
@@ -1,6 +1,6 @@
 % returns an array of randomly selected times to kill the specified 
-% number of landmarks
-% NOTE: death of certain, less used landmarks may not be noticed
+% number of randomly chosen landmarks
+% NOTE: death of certain, less used landmarks can go unnoticed
 %       should think of better way to select landmarks to kill
 function times_of_death = kill_landmarks(n_landmarks, n_killed, t0, tmax, deltaT)
     times_of_death = zeros(n_landmarks, 1); % times each landmark is killed
